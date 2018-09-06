f63fb2be213bf838435400fafa07b17a32d4e3b2
Jordan Mor <jordanmor@gmail.com>
2018-09-06 11:53:07 -0400

    Build out the Player and Token components

---
+++ js/Player.js
@@ -0,0 +1,16 @@
+class Player {
+  constructor(name, id, color, active = false) {
+    this.name = name;
+    this.id = id;
+    this.color = color;
+    this.active = active;
+    this.tokens = this.createTokens(21);
+  }
+  createTokens(num) {
+    const tokens = [];
+    for(let i = 0; i < num; i++) {
+      tokens.push(new Token(i, this));
+    }
+    return tokens;
+  }
+}
